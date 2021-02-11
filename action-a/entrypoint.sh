f92bd01edba633c006678fa0f8854ccea2bbd42f
relliott-harness <78382621+relliott-harness@users.noreply.github.com>
2021-02-11 09:19:41 -0600

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,21 @@
-#!/bin/sh -l
+#!/bin/bash
 
-sh -c "echo Hello world my name is $INPUT_MY_NAME"
+printf "\n\n"
+echo "# Open Policy Agent"
+/opa version
+printf "\n\n"
+
+IFS=';'
+mapfile -t lines < <(echo "$INPUT_TESTS" | grep -v "^$")
+
+e_code=0
+for line in "${lines[@]}"; do
+  read -r -a args <<< "$line"
+  cmd="/opa test ${args[@]} $INPUT_OPTIONS"
+  echo " 🚀 Running: $cmd"
+  printf "\n"
+  eval "$cmd" || e_code=1
+  printf "\n\n"
+done
+
+exit $e_code
