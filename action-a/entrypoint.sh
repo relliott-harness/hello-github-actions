03595e38f29482d31aa2b140a479f32167651ba8
relliott-harness <78382621+relliott-harness@users.noreply.github.com>
2021-02-16 11:18:17 -0600

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -12,7 +12,8 @@ mapfile -t lines < <(echo "$INPUT_TESTS" | grep -v "^$")
 e_code=0
 for line in "${lines[@]}"; do
   read -r -a args <<< "$line"
-  cmd="/opa test ${args[@]} $INPUT_OPTIONS"
+#  cmd="/opa test ${args[@]} $INPUT_OPTIONS"
+  cmd="opa eval --fail-defined  -i ./rego/mypipeline.json -d ./rego/check-pipeline.rego \"data.harness.pipeline.deny\""
   echo " 🚀 Running: $cmd"
   printf "\n"
   eval "$cmd" || e_code=1
