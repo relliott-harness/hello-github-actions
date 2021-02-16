024511dd110c035b07af76b608a583cf01b14cd9
relliott-harness <78382621+relliott-harness@users.noreply.github.com>
2021-02-16 12:02:30 -0600

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -13,10 +13,15 @@ e_code=0
 for line in "${lines[@]}"; do
   read -r -a args <<< "$line"
 #  cmd="/opa test ${args[@]} $INPUT_OPTIONS"
-  cmd="/opa eval --fail-defined  -i ./rego/mypipeline.yaml -d ./rego/check-pipeline.rego \"data.harness.pipeline.deny\""
+  cmd="/opa eval -f values -i ./rego/mypipeline.yaml -d ./rego/check-pipeline.rego \"data.harness.pipeline.deny\""
   echo " 🚀 Running: $cmd"
   printf "\n"
-  eval "$cmd" || e_code=1
+  #eval "$cmd" || e_code=1
+  export OPA_EVAL = /opa eval -f values -i ./rego/mypipeline.yaml -d ./rego/check-pipeline.rego "data.harness.pipeline.deny" | grep -P ": true" -o`
+  if [ $OPA_EVAL != ": true" ]
+  then
+      e_code = 1
+  fi
   printf "\n\n"
 done
 
